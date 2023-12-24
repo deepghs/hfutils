da8a2639abb8b32e88ef44e5178f9a1b59f923f2
narugo1992 <narugo992@gmail.com>
2023-12-24 22:44:23 +0800

    dev(narugo): simply tar file

--- hfutils/archive/tar.py
+++ hfutils/archive/tar.py
@@ -1,5 +1,6 @@
 import copy
 import os.path
+import tarfile
 from functools import partial
 from typing import Literal
 
@@ -36,32 +37,7 @@ except ImportError:
 CompressTyping = Literal['', 'gzip', 'bzip2', 'xz']
 
 
-def _get_gid(name):
-    if getgrnam is None or name is None:
-        return None
-    try:
-        result = getgrnam(name)
-    except KeyError:
-        result = None
-    if result is not None:
-        return result[2]
-    return None
-
-
-def _get_uid(name):
-    if getpwnam is None or name is None:
-        return None
-    try:
-        result = getpwnam(name)
-    except KeyError:
-        result = None
-    if result is not None:
-        return result[2]
-    return None
-
-
-def _tarfile_pack(directory, tar_file, compress: CompressTyping = "gzip",
-                  owner=None, group=None, silent: bool = False):
+def _tarfile_pack(directory, tar_file, compress: CompressTyping = "gzip", silent: bool = False):
     if compress is None:
         tar_compression = ''
     elif compress == 'gzip':
@@ -74,33 +50,19 @@ def _tarfile_pack(directory, tar_file, compress: CompressTyping = "gzip",
         raise ValueError("bad value for 'compress', or compression format not "
                          "supported : {0}".format(compress))
 
-    import tarfile  # late import for breaking circular dependency
-
-    uid = _get_uid(owner)
-    gid = _get_gid(group)
-
-    def _set_uid_gid(tarinfo):
-        if gid is not None:
-            tarinfo.gid = gid
-            tarinfo.gname = group
-        if uid is not None:
-            tarinfo.uid = uid
-            tarinfo.uname = owner
-        return tarinfo
-
     with tarfile.open(tar_file, f'w|{tar_compression}') as tar:
         progress = tqdm(walk_files(directory), silent=silent, desc=f'Packing {directory!r} ...')
         for file in progress:
             progress.set_description(file)
-            tar.add(os.path.join(directory, file), file, filter=_set_uid_gid)
+            tar.add(os.path.join(directory, file), file)
 
 
 def _tarfile_unpack(tar_file, directory, silent: bool = False, numeric_owner=False):
-    import tarfile  # late import for breaking circular dependency
-
     with tarfile.open(tar_file) as tar:
         directories = []
-        for tarinfo in tar:
+        progress = tqdm(tar, silent=silent, desc=f'Unpacking {directory!r} ...')
+        for tarinfo in progress:
+            progress.set_description(tarinfo.name)
             if tarinfo.isdir():
                 # Extract directories with a safe mode.
                 directories.append(tarinfo)
@@ -115,7 +77,9 @@ def _tarfile_unpack(tar_file, directory, silent: bool = False, numeric_owner=Fal
         directories.reverse()
 
         # Set correct owner, mtime and filemode on directories.
-        for tarinfo in directories:
+        progress = tqdm(directories, silent=silent, desc=f'Applying directories ...')
+        for tarinfo in progress:
+            progress.set_description(tarinfo.name)
             dirpath = os.path.join(directory, tarinfo.name)
             tar.chown(tarinfo, dirpath, numeric_owner=numeric_owner)
             tar.utime(tarinfo, dirpath)
