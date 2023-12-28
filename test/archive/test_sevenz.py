915d7dab5f9f85d6b48fa6c9c101c8dbeb34b585
narugo1992 <narugo992@gmail.com>
2023-12-28 12:25:54 +0800

    dev(narugo): add 7z test

--- test/archive/test_sevenz.py
+++ test/archive/test_sevenz.py
@@ -1,11 +1,16 @@
 import os.path
+from unittest import skipUnless
 
-import py7zr
 import pytest
 from hbutils.testing import isolated_directory, disable_output
 
 from hfutils.archive import get_archive_type, get_archive_extname, archive_pack, archive_unpack
 from test.testings import get_testfile
+
+try:
+    import py7zr
+except ImportError:  # pragma: no cover
+    py7zr = None
 
 
 @pytest.fixture()
@@ -15,13 +20,28 @@ def raw_7z():
 
 @pytest.mark.unittest
 class TestArchive7z:
+    @skipUnless(py7zr, 'py7zr module required.')
     def test_get_archive_type(self):
         assert get_archive_type(os.path.join('1.7z')) == '7z'
         assert get_archive_type(os.path.join('111', 'f.7z')) == '7z'
 
+    @skipUnless(not py7zr, 'No py7zr module required.')
+    def test_get_archive_type_no_7z(self):
+        with pytest.raises(ValueError):
+            _ = get_archive_type(os.path.join('1.7z'))
+        with pytest.raises(ValueError):
+            _ = get_archive_type(os.path.join('111', 'f.7z'))
+
+    @skipUnless(py7zr, 'py7zr module required.')
     def test_get_archive_extname(self):
         assert get_archive_extname('7z') == '.7z'
 
+    @skipUnless(not py7zr, 'No py7zr module required.')
+    def test_get_archive_extname_no_7z(self):
+        with pytest.raises(ValueError):
+            _ = get_archive_extname('7z')
+
+    @skipUnless(py7zr, 'py7zr module required.')
     def test_archive_pack(self, raw_dir, check_unpack_dir):
         with isolated_directory():
             with disable_output():
@@ -32,6 +52,7 @@ class TestArchive7z:
                 zf.extractall('dst')
             check_unpack_dir('dst')
 
+    @skipUnless(py7zr, 'py7zr module required.')
     def test_archive_unpack(self, raw_7z, check_unpack_dir):
         with isolated_directory():
             with disable_output():
