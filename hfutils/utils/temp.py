4a69378814171a1e7b8c9b3db0af459d4d6a5e51
narugo1992 <narugo992@gmail.com>
2023-12-29 14:41:13 +0800

    dev(narugo): do not use empty tmp dir

--- hfutils/utils/temp.py
+++ hfutils/utils/temp.py
@@ -13,6 +13,6 @@ def _create_tmp_dir(dir_):
 class TemporaryDirectory(_OriginTemporaryDirectory):
     # noinspection PyShadowingBuiltins
     def __init__(self, suffix=None, prefix=None, dir=None, ignore_cleanup_errors=False):
-        dir = dir or os.environ.get('TMPDIR')
+        dir = dir or os.environ.get('TMPDIR') or None
         _create_tmp_dir(dir)
         _OriginTemporaryDirectory.__init__(self, suffix, prefix, dir, ignore_cleanup_errors)
