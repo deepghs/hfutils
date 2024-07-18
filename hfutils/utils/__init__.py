fd6c1a8c621d339a0b1504b6e9bac8a7752a1cce
narugo1992 <narugo992@gmail.com>
2024-07-19 01:09:43 +0800

    dev(narugo): reformat

--- hfutils/utils/__init__.py
+++ hfutils/utils/__init__.py
@@ -1,7 +1,7 @@
 from .binary import is_binary_file
 from .download import download_file
+from .logging import ColoredFormatter
 from .path import hf_normpath, hf_fs_path, parse_hf_fs_path, HfFileSystemPath
 from .temp import TemporaryDirectory
 from .tqdm_ import tqdm
 from .walk import walk_files
-from .logging import ColoredFormatter
