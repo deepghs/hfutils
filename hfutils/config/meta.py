f23f910f505a1b16b03c53337f597f5d916a50e2
narugo1992 <narugo992@gmail.com>
2024-07-21 15:37:16 +0800

    dev(narugo): use version v0.3.0

--- hfutils/config/meta.py
+++ hfutils/config/meta.py
@@ -7,7 +7,7 @@ Overview:
 __TITLE__ = 'hfutils'
 
 #: Version of this project.
-__VERSION__ = '0.2.11'
+__VERSION__ = '0.3.0'
 
 #: Short description of the project, will be included in ``setup.py``.
 __DESCRIPTION__ = 'Useful utilities for huggingface'
