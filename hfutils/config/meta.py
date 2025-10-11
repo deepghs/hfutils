9e2a70828dedd0b39c830450dc88a9aec3a0f532
narugo1992 <narugo992@gmail.com>
2025-10-11 19:00:40 +0800

    dev(narugo): try use readthedocs, ci skip, use version v0.12.2

--- hfutils/config/meta.py
+++ hfutils/config/meta.py
@@ -7,7 +7,7 @@ Overview:
 __TITLE__ = 'hfutils'
 
 #: Version of this project.
-__VERSION__ = '0.12.1'
+__VERSION__ = '0.12.2'
 
 #: Short description of the project, will be included in ``setup.py``.
 __DESCRIPTION__ = 'Useful utilities for huggingface'
